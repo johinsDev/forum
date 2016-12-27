dd05717d37247f95e25bf0a840147e6c74908d98
johinsDev <johinsdev@gmail.com>
2016-12-27 23:50:42 +0000

    Use MYSQL_USER instead of MYSQL_USERNAME

--- config/database.php
+++ config/database.php
@@ -65,7 +65,7 @@ return [
                 'host' => env('DB_HOST', '127.0.0.1'),
                 'port' => env('DB_PORT', '3306'),
                 'database' => env('DB_DATABASE_TEST', 'forum_tests'),
-                'username' => env('MYSQL_USERNAME' , env('DB_USERNAME', 'forge')),
+                'username' => env('MYSQL_USER' , env('DB_USERNAME', 'forge')),
                 'password' => env('MYSQL_PASSWORD' , env('DB_PASSWORD', '')),
                 'charset' => 'utf8',
                 'collation' => 'utf8_unicode_ci',
